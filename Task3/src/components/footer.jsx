8a1e847d1ba4862da2c9996958cc8968db7b4694
Pranjallpandey1504 <pranjal1504pandey@gmail.com>
2025-02-11 23:45:49 +0530

    2203031080144-assignment3

---
+++ Task3/src/components/footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <footer>© 2025 Pranjal</footer>;
+}
+
+export default Footer;
---
+++ Task3/src/components/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <h2>Main Content</h2>
+      <p>Welcome to my Portfolio</p>
+    </main>
+  );
+}
+
+export default Main;
